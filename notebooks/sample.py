b81cd23f0f3d3234229ff3f9832fa947f5328370
Harry45 <arrykrish@gmail.com>
2023-08-28 20:20:32 +0100

    sampler

--- notebooks/sample.py
+++ notebooks/sample.py
@@ -17,60 +17,126 @@ import numpyro.infer.autoguide as autoguide
 
 # our script
 from cosmology.samplenumpyro import numpyro_model
-from cosmology.bandpowers import get_nz, scale_cuts, extract_bandwindow, extract_data_covariance, get_params_vec
+from cosmology.bandpowers import (
+    get_nz,
+    scale_cuts,
+    extract_bandwindow,
+    extract_data_covariance,
+    get_params_vec,
+)
 from cosmology.sampleemcee import jit_theory, emcee_logpost
 
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file("config", None, "Main configuration file.")
 
-def get_test_param():
-    cosmo = jc.Cosmology(sigma8=0.852, Omega_c = 0.239, Omega_b=0.054,
-                        h = 0.653, n_s=0.933, w0=-1., Omega_k=0., wa=0.)
 
-    parameter = get_params_vec(cosmo,
-                        [0.074, 0.186, -0.075, -0.108],
-                        [-0.008, -0.100, -0.0018, -0.0097],
-                        [0.359, -0.011],
-                        [1.34, 1.58, 1.60, 1.90, 1.94],
-                        [0.022, -0.0015, 0.02, 0.0097, -0.012])
+def get_test_param():
+    cosmo = jc.Cosmology(
+        sigma8=0.852,
+        Omega_c=0.239,
+        Omega_b=0.054,
+        h=0.653,
+        n_s=0.933,
+        w0=-1.0,
+        Omega_k=0.0,
+        wa=0.0,
+    )
+
+    parameter = get_params_vec(
+        cosmo,
+        [0.074, 0.186, -0.075, -0.108],
+        [-0.008, -0.100, -0.0018, -0.0097],
+        [0.359, -0.011],
+        [1.34, 1.58, 1.60, 1.90, 1.94],
+        [0.022, -0.0015, 0.02, 0.0097, -0.012],
+    )
     return parameter
 
 
-def load_data(fname = 'cls_DESY1', kmax = 0.15, lmin_wl = 30, lmax_wl = 2000):
+def load_data(fname="cls_DESY1", kmax=0.15, lmin_wl=30, lmax_wl=2000):
     saccfile = sacc.Sacc.load_fits(f"data/{fname}.fits")
     jax_nz_wl = get_nz(saccfile, tracertype="wl")
     jax_nz_gc = get_nz(saccfile, tracertype="gc")
-    saccfile_cut = scale_cuts(saccfile, kmax = kmax, lmin_wl = lmin_wl, lmax_wl = lmax_wl)
+    saccfile_cut = scale_cuts(saccfile, kmax=kmax, lmin_wl=lmin_wl, lmax_wl=lmax_wl)
     bw_gc, bw_gc_wl, bw_wl = extract_bandwindow(saccfile_cut)
     data, covariance = extract_data_covariance(saccfile_cut)
     precision = jnp.linalg.inv(covariance)
-    newcov = covariance + jnp.eye(data.shape[0]) * 1E-18
+    newcov = covariance + jnp.eye(data.shape[0]) * 1e-18
     newprec = jnp.linalg.inv(newcov)
     return data, newprec, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl
 
 
 def sampling_nuts(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg):
-    nuts_kernel = NUTS(numpyro_model, step_size=cfg.nuts.stepsize, init_strategy=init_to_median, dense_mass=cfg.nuts.dense_mass, max_tree_depth=cfg.nuts.max_tree_depth)
-    mcmc_nuts = MCMC(nuts_kernel, num_warmup=cfg.nuts.nwarmup, num_samples=cfg.nuts.nsamples, num_chains=cfg.nuts.nchain,
-                chain_method=cfg.nuts.chainmethod,
-                progress_bar=True)
-    mcmc_nuts.run(jax.random.PRNGKey(cfg.nuts.rng), data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
+    nuts_kernel = NUTS(
+        numpyro_model,
+        step_size=cfg.nuts.stepsize,
+        init_strategy=init_to_median,
+        dense_mass=cfg.nuts.dense_mass,
+        max_tree_depth=cfg.nuts.max_tree_depth,
+    )
+    mcmc_nuts = MCMC(
+        nuts_kernel,
+        num_warmup=cfg.nuts.nwarmup,
+        num_samples=cfg.nuts.nsamples,
+        num_chains=cfg.nuts.nchain,
+        chain_method=cfg.nuts.chainmethod,
+        progress_bar=True,
+    )
+    mcmc_nuts.run(
+        jax.random.PRNGKey(cfg.nuts.rng),
+        data,
+        precision,
+        jax_nz_gc,
+        jax_nz_wl,
+        bw_gc,
+        bw_gc_wl,
+        bw_wl,
+    )
     return mcmc_nuts
 
+
 def sampling_barker(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg):
-    barker_kernel = BarkerMH(numpyro_model, step_size=cfg.barker.stepsize, init_strategy=init_to_median, dense_mass=cfg.barker.dense_mass)
-    mcmc_barker = MCMC(barker_kernel, num_warmup=cfg.barker.nwarmup, num_samples=cfg.barker.nsamples, num_chains=cfg.barker.nchain,
-                chain_method=cfg.barker.chainmethod, progress_bar=True)
-    mcmc_barker.run(jax.random.PRNGKey(cfg.barker.rng), data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
+    barker_kernel = BarkerMH(
+        numpyro_model,
+        step_size=cfg.barker.stepsize,
+        init_strategy=init_to_median,
+        dense_mass=cfg.barker.dense_mass,
+    )
+    mcmc_barker = MCMC(
+        barker_kernel,
+        num_warmup=cfg.barker.nwarmup,
+        num_samples=cfg.barker.nsamples,
+        num_chains=cfg.barker.nchain,
+        chain_method=cfg.barker.chainmethod,
+        progress_bar=True,
+    )
+    mcmc_barker.run(
+        jax.random.PRNGKey(cfg.barker.rng),
+        data,
+        precision,
+        jax_nz_gc,
+        jax_nz_wl,
+        bw_gc,
+        bw_gc_wl,
+        bw_wl,
+    )
     return mcmc_barker
+
 
 def sampling_emcee(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg):
     parameter = get_test_param()
     test_theory = jit_theory(parameter, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
     nparams = len(parameter)
-    pos = parameter + cfg.emcee.eps * jax.random.normal(jax.random.PRNGKey(0), (2*nparams, nparams))
+    pos = parameter + cfg.emcee.eps * jax.random.normal(
+        jax.random.PRNGKey(0), (2 * nparams, nparams)
+    )
     nwalkers, ndim = pos.shape
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, emcee_logpost, args=(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl))
+    sampler = emcee.EnsembleSampler(
+        nwalkers,
+        ndim,
+        emcee_logpost,
+        args=(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl),
+    )
     sampler.run_mcmc(pos, cfg.emcee.nsamples, progress=True)
     return sampler
 
@@ -83,14 +149,23 @@ def main(_):
     jc.power.USE_EMU = cfg.use_emu
 
     # run the sampler
-    data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl = load_data(fname = 'cls_DESY1', kmax = 0.15, lmin_wl = 30, lmax_wl = 2000)
+    data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl = load_data(
+        fname="cls_DESY1", kmax=0.15, lmin_wl=30, lmax_wl=2000
+    )
 
-    if cfg.sampler == 'nuts':
-        mcmc_nuts = sampling_nuts(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg)
-    elif cfg.sampler == 'barker':
-        mcmc_barker = sampling_barker(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg)
-    elif cfg.sampler == 'emcee':
-        mcmc_emcee = sampling_emcee(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg)
+    if cfg.sampler == "nuts":
+        mcmc_nuts = sampling_nuts(
+            data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg
+        )
+    elif cfg.sampler == "barker":
+        mcmc_barker = sampling_barker(
+            data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg
+        )
+    elif cfg.sampler == "emcee":
+        mcmc_emcee = sampling_emcee(
+            data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl, cfg
+        )
+
 
 if __name__ == "__main__":
     app.run(main)
--- notebooks/sampledynesty.py
+++ notebooks/sampledynesty.py
@@ -5,6 +5,7 @@ from ml_collections.config_dict import ConfigDict
 import jax
 import jax.numpy as jnp
 import jax_cosmo as jc
+import jax.scipy.stats.norm as jxnorm
 from dynesty import NestedSampler
 
 # our script
@@ -14,27 +15,44 @@ from sample import load_data
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file("config_ns", None, "Main configuration file.")
 
-data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl = load_data(fname = 'cls_DESY1', kmax = 0.15, lmin_wl = 30, lmax_wl = 2000)
+data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl = load_data(
+    fname="cls_DESY1", kmax=0.15, lmin_wl=30, lmax_wl=2000
+)
 
 
 @jax.jit
 def jit_theory(parameters, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl):
-    return get_bandpowers_theory(parameters, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
+    return get_bandpowers_theory(
+        parameters, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl
+    )
+
 
 def get_test_param():
-    cosmo = jc.Cosmology(sigma8=0.852, Omega_c = 0.239, Omega_b=0.054,
-                        h = 0.653, n_s=0.933, w0=-1., Omega_k=0., wa=0.)
+    cosmo = jc.Cosmology(
+        sigma8=0.852,
+        Omega_c=0.239,
+        Omega_b=0.054,
+        h=0.653,
+        n_s=0.933,
+        w0=-1.0,
+        Omega_k=0.0,
+        wa=0.0,
+    )
 
-    testparameter = get_params_vec(cosmo,
-                        [0.074, 0.186, -0.075, -0.108],
-                        [-0.008, -0.100, -0.0018, -0.0097],
-                        [0.359, -0.011],
-                        [1.34, 1.58, 1.60, 1.90, 1.94],
-                        [0.022, -0.0015, 0.02, 0.0097, -0.012])
+    testparameter = get_params_vec(
+        cosmo,
+        [0.074, 0.186, -0.075, -0.108],
+        [-0.008, -0.100, -0.0018, -0.0097],
+        [0.359, -0.011],
+        [1.34, 1.58, 1.60, 1.90, 1.94],
+        [0.022, -0.0015, 0.02, 0.0097, -0.012],
+    )
     return testparameter
+
 
 def unit_transform(random_number, xmin, xmax):
     return xmin + random_number * (xmax - xmin)
+
 
 def dynesty_prior(unit_cube):
     parameter = jnp.array(unit_cube)
@@ -47,16 +65,16 @@ def dynesty_prior(unit_cube):
     parameter = parameter.at[4].set(unit_transform(parameter[4], 0.87, 1.07))
 
     # multiplicative
-    parameter = parameter.at[5].set(jax.scipy.stats.norm.ppf(parameter[5], 0.012, 0.023))
-    parameter = parameter.at[6].set(jax.scipy.stats.norm.ppf(parameter[6], 0.012, 0.023))
-    parameter = parameter.at[7].set(jax.scipy.stats.norm.ppf(parameter[7], 0.012, 0.023))
-    parameter = parameter.at[8].set(jax.scipy.stats.norm.ppf(parameter[8], 0.012, 0.023))
+    parameter = parameter.at[5].set(jxnorm.ppf(parameter[5], 0.012, 0.023))
+    parameter = parameter.at[6].set(jxnorm.ppf(parameter[6], 0.012, 0.023))
+    parameter = parameter.at[7].set(jxnorm.ppf(parameter[7], 0.012, 0.023))
+    parameter = parameter.at[8].set(jxnorm.ppf(parameter[8], 0.012, 0.023))
 
     # shifts
-    parameter = parameter.at[9].set(jax.scipy.stats.norm.ppf(parameter[9], -0.001, 0.016))
-    parameter = parameter.at[10].set(jax.scipy.stats.norm.ppf(parameter[10], -0.019, 0.013))
-    parameter = parameter.at[11].set(jax.scipy.stats.norm.ppf(parameter[11], 0.009, 0.011))
-    parameter = parameter.at[12].set(jax.scipy.stats.norm.ppf(parameter[12], -0.018, 0.022))
+    parameter = parameter.at[9].set(jxnorm.ppf(parameter[9], -0.001, 0.016))
+    parameter = parameter.at[10].set(jxnorm.ppf(parameter[10], -0.019, 0.013))
+    parameter = parameter.at[11].set(jxnorm.ppf(parameter[11], 0.009, 0.011))
+    parameter = parameter.at[12].set(jxnorm.ppf(parameter[12], -0.018, 0.022))
 
     # intrinsic alignment
     parameter = parameter.at[13].set(unit_transform(parameter[13], -1.0, 1.0))
@@ -70,30 +88,32 @@ def dynesty_prior(unit_cube):
     parameter = parameter.at[19].set(unit_transform(parameter[19], 0.8, 3.0))
 
     # shifts (galaxy clustering)
-    parameter = parameter.at[20].set(jax.scipy.stats.norm.ppf(parameter[20], 0.0, 0.007))
-    parameter = parameter.at[21].set(jax.scipy.stats.norm.ppf(parameter[21], 0.0, 0.007))
-    parameter = parameter.at[22].set(jax.scipy.stats.norm.ppf(parameter[22], 0.0, 0.006))
-    parameter = parameter.at[23].set(jax.scipy.stats.norm.ppf(parameter[23], 0.0, 0.01))
-    parameter = parameter.at[24].set(jax.scipy.stats.norm.ppf(parameter[24], 0.0, 0.01))
+    parameter = parameter.at[20].set(jxnorm.ppf(parameter[20], 0.0, 0.007))
+    parameter = parameter.at[21].set(jxnorm.ppf(parameter[21], 0.0, 0.007))
+    parameter = parameter.at[22].set(jxnorm.ppf(parameter[22], 0.0, 0.006))
+    parameter = parameter.at[23].set(jxnorm.ppf(parameter[23], 0.0, 0.01))
+    parameter = parameter.at[24].set(jxnorm.ppf(parameter[24], 0.0, 0.01))
     return parameter
+
 
 def dynesty_loglike(parameters):
     theory = jit_theory(parameters, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
     diff = data - theory
     chi2 = diff @ newprec @ diff
     isnan = jnp.isnan(chi2)
-    chi2 = jnp.where(isnan, 1E32, chi2)
-    return -0.5*chi2
+    chi2 = jnp.where(isnan, 1e32, chi2)
+    return -0.5 * chi2
+
 
 def main(_):
     cfg = FLAGS.config_ns
     parameter = get_test_param()
-    # python sampledynesty.py --config_ns=config.py:desyr1 --config_ns.dynesty.nlive=100
     print(cfg.dynesty.nlive, cfg.dynesty.ndim)
     # test_theory = jit_theory(parameter, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl)
     # des_sampler = NestedSampler(dynesty_loglike, dynesty_prior, ndim=cfg.dynesty.ndim, nlive=cfg.dynesty.nlive)
     # des_sampler.run_nested(maxiter = 200) # to remove this - dynesty should run until converged
     # # pickle_save(des_sampler, 'samples', 'des_sampler_test')
+
 
 if __name__ == "__main__":
     app.run(main)
