182475db4bcb6b34c9d39ec58b590fb6f52e6465
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2023-08-29 14:32:59 +0100

    test

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,4 +1,13 @@
 import jax
+import jaxlib
+
+
+num_devices = jax.device_count()
+device_type = jax.devices()[0].device_kind
+
+print(f"jax version: {jax.__version__}")
+print(f"jaxlib version: {jaxlib.__version__}")
+print(f"Found {num_devices} JAX devices of type {device_type}.")
 
 jax.default_device = jax.devices("gpu")[1]
 jax.devices()
