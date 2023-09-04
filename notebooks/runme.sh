fbef8b66c2174fb16a7c2d5b4cab7f2d5ce03aac
Harry45 <arrykrish@gmail.com>
2023-09-04 15:42:46 +0100

    runme script

--- notebooks/runme.sh
+++ notebooks/runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo
-python sample.py --config=config.py:desyr1 --config.sampler=nuts --config.nuts.nsamples=50 --config.use_emu=False --config.samplername=1
+python sample.py --config=config.py:desyr1 --config.sampler=nuts --config.nuts.nsamples=50 --config.use_emu=True --config.samplername=1
