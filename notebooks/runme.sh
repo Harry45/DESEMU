659cc24284a3ad49b6582879b94d3abb0ebc2c29
Harry45 <arrykrish@gmail.com>
2023-09-06 15:13:21 +0100

    runme script

--- notebooks/runme.sh
+++ notebooks/runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo
-python sample.py --config=config.py:desyr1 --config.sampler=barker --config.barker.nwarmup=500 --config.barker.nsamples=15000 --config.barker.nchain=2 --config.barker.chainmethod=sequential --config.use_emu=False --config.samplername=1
+python sample.py --config=config.py:desyr1 --config.sampler=barker --config.barker.nwarmup=500 --config.barker.nsamples=15000 --config.barker.nchain=1 --config.barker.chainmethod=sequential --config.use_emu=False --config.samplername=1
