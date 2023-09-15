b5bb339cb4d7f3d5197e2cd9c2f6a74ddc9ef0da
Harry45 <arrykrish@gmail.com>
2023-09-15 08:59:45 +0100

    ccl run

--- notebooks/runme.sh
+++ notebooks/runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo
-python sampleccl.py --configccl=config.py:desyr1 --configccl.sampler=cclemcee --configccl.ccl.nsamples=2 --configccl.samplername=test
+python sampleccl.py --configccl=config.py:desyr1 --configccl.sampler=cclemcee --configccl.ccl.nsamples=5000 --configccl.samplername=1
 # python sample.py --config=config.py:desyr1 --config.sampler=barker --config.barker.nwarmup=1000 --config.barker.nsamples=150000 --config.use_emu=False --config.samplername=2
 # python sample.py --config=config.py:desyr1 --config.sampler=barker --config.barker.nwarmup=1000 --config.barker.nsamples=150000 --config.use_emu=True --config.samplername=1
 # python sample.py --config=config.py:desyr1 --config.sampler=barker --config.barker.nwarmup=1000 --config.barker.nsamples=150000 --config.use_emu=True --config.samplername=2
