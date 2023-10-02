819d477eeec88c5e4e285f3abc139704f3d54155
Harry45 <arrykrish@gmail.com>
2023-10-02 14:34:35 +0100

    run script

--- notebooks/runns_emu_1.sh
+++ notebooks/runns_emu_1.sh
@@ -3,5 +3,6 @@ source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 echo Sampling with dynesty
+
 python sampledynesty.py --config_ns=config.py:desyr1 --config_ns.sampler=ns --config_ns.use_emu=True --config_ns.dynesty.nlive=1500 --config_ns.samplername=1
 # addqueue -q cmbgpu -n 1x4 -m 5 -s ./runns_emu_1.sh
