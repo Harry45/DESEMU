8959ad9e46d1fa70ae6e163467ecca068d385769
Harry45 <arrykrish@gmail.com>
2023-10-02 13:58:41 +0100

    run script

--- notebooks/runns_1.sh
+++ notebooks/runns_1.sh
@@ -3,4 +3,4 @@ source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 python sampledynesty.py --config_ns=config.py:desyr1 --config_ns.sampler=ns --config_ns.use_emu=False --config_ns.dynesty.nlive=1500 --config_ns.samplername=2
-# addqueue -q gpucmb -n 1x4 -m 5 -s ./runns_1.sh
+# addqueue -q cmbgpu -n 1x4 -m 5 -s ./runns_1.sh
