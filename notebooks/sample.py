0e508313f0d6ae1cdccb1dcda091ed113491b6ef
Harry45 <arrykrish@gmail.com>
2023-12-23 10:05:18 +0000

    sample script

--- notebooks/sample.py
+++ notebooks/sample.py
@@ -111,6 +111,8 @@ def sampling_nuts(data, precision, jax_nz_gc, jax_nz_wl, bw_gc, bw_gc_wl, bw_wl,
         extra_fields=("potential_energy", "num_steps"),
     )
     save_sampler(mcmc_nuts, cfg)
+
+    print("NUTS Sampling completed")
     return mcmc_nuts
 
 
