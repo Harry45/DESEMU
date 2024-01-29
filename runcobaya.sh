6cb8b1b9d7ad98839f810edd27e9b3763e3e9b1e
Harry45 <arrykrish@gmail.com>
2024-01-29 10:27:14 +0000

    cobaya test

--- runcobaya.sh
+++ runcobaya.sh
@@ -2,9 +2,6 @@ source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
+python samplecobaya.py
 
-bins=$1
-fname_out=$2​
-export OMP_NUM_THREADS=4
-
-/usr/local/shared/slurm/bin/srun -N 2 -n 2 --ntasks-per-node 3 -m cyclic --mpi=pmi2 python samplecobaya.py
+# addqueue -n 2x4 -s -q cmb -c cobaya_test -m 1 ./runcobaya.sh
