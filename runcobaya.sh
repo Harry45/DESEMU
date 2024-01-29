56c928b63377d751708b89aa1fe4fcf2e14c142c
Harry45 <arrykrish@gmail.com>
2024-01-29 10:44:27 +0000

    cobaya test

--- runcobaya.sh
+++ runcobaya.sh
@@ -3,8 +3,8 @@ conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 
-/usr/local/shared/slurm/bin/srun -N 2 -n 2 --ntasks-per-node 3 -m cyclic --mpi=pmi2 /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo/bin/python samplecobaya.py
+# /usr/local/shared/slurm/bin/srun -N 2 -n 2 --ntasks-per-node 3 -m cyclic --mpi=pmi2 /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo/bin/python samplecobaya.py
 
-# python samplecobaya.py
+python samplecobaya.py
 
 # addqueue -n 2x4 -s -q cmb -c cobaya_test -m 1 ./runcobaya.sh
