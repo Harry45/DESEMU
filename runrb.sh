f1827f3cf121ef5a8c9c1f13a4073343ae1377d7
Harry45 <arrykrish@gmail.com>
2024-02-02 13:58:50 +0000

    run script for rosenbrock

---
+++ runrb.sh
@@ -0,0 +1,5 @@
+source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
+conda activate jaxcosmo
+which python
+echo $(for i in $(seq 1 50); do printf "-"; done)
+python rosenbrock-factor-varied.py
