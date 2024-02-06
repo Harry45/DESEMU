187c549f75ebc632e6c6e33811e7a6a9cfefa960
Harry45 <arrykrish@gmail.com>
2024-02-06 09:41:48 +0000

    rosenbrock experiment

--- rosenbrock-factor-varied.py
+++ rosenbrock-factor-varied.py
@@ -152,7 +152,8 @@ def process_nuts_chains(mcmc, ndim=ndim):
 
 if __name__ == "__main__":
     # factors = np.geomspace(10, 40, 6, endpoint=True)
-    factors = np.geomspace(1e-3, 1, 6, endpoint=True)
+    # factors = np.geomspace(1e-3, 1, 6, endpoint=True)
+    factors = np.geomspace(2, 9, 6, endpoint=True)
     nrepeat = 5
     repetition = []
 
@@ -186,11 +187,11 @@ if __name__ == "__main__":
             gc.collect()
 
         repetition.append(record)
-        dill_save(record, "rosenbrock", f"repetition_2_{r}")
+        dill_save(record, "rosenbrock", f"repetition_3_{r}")
         jax.clear_backends()
 
     dill_save(
         {"gamma": repetition, "factors": factors},
         "rosenbrock",
-        "d_26_different_factors_2",
+        "d_26_different_factors_3",
     )
