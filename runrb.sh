7d1d75af44371ee98410e06e604a6d3c742d6c6c
Harry45 <arrykrish@gmail.com>
2024-02-07 08:41:02 +0000

    run script for rosenbrock

--- runrb.sh
+++ runrb.sh
@@ -2,5 +2,6 @@ source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
-python rosenbrock-factor-varied.py
+python rosenbrock.py
+# python rosenbrock-factor-varied.py
 # addqueue -s -q gpulong -m 10 --gpus 1 ./runrb.sh
