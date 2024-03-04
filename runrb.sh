09c86b98ff819885dd62d41e14fdcefa38a1e69d
Harry45 <arrykrish@gmail.com>
2024-03-04 10:29:20 +0000

    rosenbrock

--- runrb.sh
+++ runrb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo $PWD
-source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
-source activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 python rosenbrock.py
 
+# source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
+# source activate jaxcosmo
 # addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runrb.sh
