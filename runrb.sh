a1dba27b13bf33e598a027332cfc007a5467ec93
Harry45 <arrykrish@gmail.com>
2024-03-04 11:32:59 +0000

    Rosenbrock Function

--- runrb.sh
+++ runrb.sh
@@ -7,4 +7,4 @@ which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 python rosenbrock.py
 
-# addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx3070with8gb ./runrb.sh
+# addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runrb.sh
--- test.py
+++
@@ -1,8 +0,0 @@
-import numpyro
-import jax
-import jax.numpy as jnp
-import numpyro.distributions as dist
-from numpyro.infer import MCMC, NUTS, init_to_value
-from numpyro.diagnostics import summary
-
-print("Harry is happy")
