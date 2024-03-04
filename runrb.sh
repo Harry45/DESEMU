dc655b2ad1f50a2dc7809382cbe753044d645452
Harry45 <arrykrish@gmail.com>
2024-03-04 10:26:41 +0000

    Rosenbrock Experiment

--- runrb.sh
+++ runrb.sh
@@ -2,11 +2,8 @@
 echo $PWD
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 source activate jaxcosmo
-python test.py
-# source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
-# conda activate jaxcosmo
-# which python
-# echo $(for i in $(seq 1 50); do printf "-"; done)
-# python rosenbrock.py
-# python rosenbrock-factor-varied.py
+which python
+echo $(for i in $(seq 1 50); do printf "-"; done)
+python rosenbrock.py
+
 # addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runrb.sh
