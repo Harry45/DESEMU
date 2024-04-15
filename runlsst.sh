8aa20cdd68a2dd990a0236d32359ef8a32a3261e
Harry45 <arrykrish@gmail.com>
2024-04-15 14:32:29 +0100

    runlsst

--- runlsst.sh
+++ runlsst.sh
@@ -5,6 +5,4 @@ which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 python lsstnutssampler.py
 
-# /usr/local/shared/slurm/bin/srun -N 2 -n 2 --ntasks-per-node 3 -m cyclic --mpi=pmi2 /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo/bin/python samplecobaya.py
-# addqueue -n 2x4 -s -q cmb -c cobaya_test -m 1 ./runcobaya.sh
-# addqueue -s -q gpulong -m 10 --gpus 1 ./runlsst.sh
+# addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runrb.sh
