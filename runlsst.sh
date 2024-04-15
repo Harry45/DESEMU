2eae6c229324c4a43f444b357b9371515b16b319
Harry45 <arrykrish@gmail.com>
2024-04-15 14:32:51 +0100

    runlsst

--- runlsst.sh
+++ runlsst.sh
@@ -5,4 +5,4 @@ which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 python lsstnutssampler.py
 
-# addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runrb.sh
+# addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runlsst.sh
