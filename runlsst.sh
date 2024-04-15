34bcf41211f30d89f4652df8f98803a61b09dcb7
Harry45 <arrykrish@gmail.com>
2024-04-15 14:53:15 +0100

    lsst

--- runlsst.sh
+++ runlsst.sh
@@ -3,6 +3,7 @@ source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
-python lsstnutssampler.py
+python lsstcobayasampler.py
+# python lsstnutssampler.py
 
 # addqueue -s -q gpulong -m 10 --gpus 1 --gputype rtx2080with12gb ./runlsst.sh
