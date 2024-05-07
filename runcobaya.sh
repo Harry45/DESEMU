63fee63f18c13f692cf32c76dc325addbdb61512
Harry45 <arrykrish@gmail.com>
2024-05-07 09:22:10 +0100

    runcobaya

--- runcobaya.sh
+++ runcobaya.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 export COBAYA_USE_FILE_LOCKING=false
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
 conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
-python samplecobaya.py
+time python samplecobaya.py
 
 # /usr/local/shared/slurm/bin/srun -N 2 -n 2 --ntasks-per-node 3 -m cyclic --mpi=pmi2 /mnt/zfsusers/phys2286/anaconda3/envs/jaxcosmo/bin/python samplecobaya.py
 # addqueue -n 2x4 -s -q cmb -c cobaya_test -m 1 ./runcobaya.sh
